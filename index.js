ce793b0e026cbc8164cf4dc5bb9fe75f89b2e2dd
Jhon Yaguana <jyaguana3@utmachala.edu.ec>
2023-07-12 01:10:28 -0500

    sdd

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 app.set('port', process.env.PORT || 4000);
 app.set('views',path.join(__dirname,'views'));
 
-app.engine('.hbs', exphbs.engine({
+app.engine('.hbs', exphbs({
     defaultLayouts:'main',
     layoutsDir:path.join(app.get('views'),'layouts'),
     partialsDir: path.join(app.get('views'),'partials'),
